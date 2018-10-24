24ecbce6193f13f745e084360fcdbc5a91def53e
qwality <low.qwality@gmail.com>
2018-10-24 23:34:40 +0200

    ...

--- __stack.py
+++ __stack.py
@@ -6,6 +6,12 @@ class Stack:
     def push(self, item):
         return Stack(item, self)
 
+    def push2(self, *items):
+        if len(items) is 1:
+            return Stack(items[0], self)
+        else:
+            return Stack(items[0], self).push2(*items[1:])
+
     def isEmpty(self):
         return self.item is None       # porad nevim jestli item nebo next
 
@@ -36,7 +42,7 @@ class Stack:
         return self.item if key is 0 else self.next[key - 1]
 
     def __str__(self):
-        return "{0!s}".format(list(self))
+        return ">{0!s}".format(list(self))
 
     def __repr__(self):
         return "{self.__class__.__name__}({self.item!s}, {self.next!r})".format(self=self)
