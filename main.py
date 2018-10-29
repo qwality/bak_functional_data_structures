e4e0e4c4984eaae9fe94972d7172f13ba1ace953
qwality <low.qwality@gmail.com>
2018-10-29 23:14:34 +0100

    some more polishing

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from __queue2 import *
+from functools import reduce
 
 
-def test(tst, fnc, lst):
+def compose(*args):
+    return reduce(lambda y, x: lambda z: x(y(z)), reversed(args))
+
+
+def test(tst, lst, fnc=lambda x: x):
     prev = tst
     for i in lst:
         if i != 0:
             prev = prev.push(i)
-            print(fnc(prev))
+            fnc(prev)
         else:
             prev = prev.pop()
-            print(fnc(prev))
+            fnc(prev)
 
     return prev
 
@@ -24,17 +29,18 @@ def main():
     print(pseudo, repr(pseudo), sep="\n")
 
     print("\n--Queue--")
-    test(Queue(), str, [1, 2, 3, 0, 0, 4, 5, 7, 8, 9, 10, 11, 0])
+    test(Queue(), [1, 2, 3, 0, 0, 4, 5, 7, 8, 9, 10, 11, 0], compose(print, str))
 
     print("\n--Queue 2--")
-    test(Queue2(), repr, [1, 2, 3, 0, 0, 4, 5, 7, 8, 9, 10, 11, 0])
+    test(Queue2(), [1, 2, 3, 0, 0, 4, 5, 7, 8, 9, 10, 11, 0], compose(print, repr))
     print("Queue2	[4, 5, 7, 8, 9, 10] [11], d=5")
 
     print("\n--tests--")
-    print(test(Queue2(), repr, [1, 2, 3, 0, 0, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]))
-
-
-
+    print(test(
+        Queue2(),
+        [1, 2, 3, 0, 0, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+        compose(print, repr)
+    ))
 
 
 if __name__ == "__main__":
